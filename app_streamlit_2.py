664fb253cd9c9bf4c324cb3283942be98205b9ce
aaloni1 <aaloni@ssct.edu.ph>
2024-12-10 10:56:13 +0800

    Update app_streamlit_2.py

--- app_streamlit_2.py
+++ app_streamlit_2.py
@@ -1,5 +1,5 @@
-import streamlit as st
-    st.markdown(
+import streamlit as st 
+st.markdown(
     """
     <h1 style="text-align: center; background: linear-gradient(to right, white, black); -webkit-background-clip: text; -webkit-text-fill-color: transparent;">Ady's Blog</h1>
     """,
