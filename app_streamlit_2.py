2fc376fcfa316792cf738e36f36af7bea2b123ae
aaloni1 <aaloni@ssct.edu.ph>
2024-12-10 10:59:54 +0800

    Update app_streamlit_2.py

--- app_streamlit_2.py
+++ app_streamlit_2.py
@@ -5,3 +5,18 @@ st.markdown(
     """,
     unsafe_allow_html=True
  )
+
+blog_posts = [
+    {
+        "content": [
+            ("Name", "Adam Rey G. Aloni"),  # Included name in content
+            ("Age", "19 years old"),
+            ("Gender", "Male"),
+            ("Birthdate", "October 27, 2005"),
+            ("Birthplace", "Quiapo, Manila"),
+            ("Permanent Address", "Purok 4 Bonifacio, Ritaglenda, Basilisa, Dinagat Islands"),
+            ("Current Address", "Brg. Taft Surigao City, Surigao Del Norte"),
+        ],
+        "image": "https://scontent.fcgy2-4.fna.fbcdn.net/v/t39.30808-6/447232820_485701057364609_6882034262964220786_n.jpg?_nc_cat=105&ccb=1-7&_nc_sid=a5f93a&_nc_eui2=AeHjriIDTRJC_PcvJAfgIouOFYiAgsWwUSoViICCxbBRKheimFO4y58FaaCHAq9X9ZazI6GTSJfowGXCwAWihCGQ&_nc_ohc=BKxVkhXz20EQ7kNvgHPdImd&_nc_zt=23&_nc_ht=scontent.fcgy2-4.fna&_nc_gid=Ai8oJezuBIWDV3zOmo6nf18&oh=00_AYC8O_UH06Ya04GiX9_yRbtBH9FcVgi15JusViQXFAD3pQ&oe=674B3112"
+    }
+]
