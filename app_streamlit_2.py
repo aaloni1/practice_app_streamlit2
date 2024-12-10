4a539c6771bb75fd5a212c837a5eeb1834f86f50
aaloni1 <aaloni@ssct.edu.ph>
2024-12-10 10:56:50 +0800

    Update app_streamlit_2.py

--- app_streamlit_2.py
+++ app_streamlit_2.py
@@ -1,7 +1,7 @@
 import streamlit as st 
 st.markdown(
     """
-    <h1 style="text-align: center; background: linear-gradient(to right, white, black); -webkit-background-clip: text; -webkit-text-fill-color: transparent;">Ady's Blog</h1>
+    <h1 style="text-align: center; background: linear-gradient(to right, purple, black); -webkit-background-clip: text; -webkit-text-fill-color: transparent;">Ady's Blog</h1>
     """,
     unsafe_allow_html=True
  )
