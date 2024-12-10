4488c949c716c37293b51f47f80e0f000c56c9b8
aaloni1 <aaloni@ssct.edu.ph>
2024-12-10 11:01:32 +0800

    Update app_streamlit_2.py

--- app_streamlit_2.py
+++ app_streamlit_2.py
@@ -20,3 +20,25 @@ blog_posts = [
         "image": "https://scontent.fcgy2-4.fna.fbcdn.net/v/t39.30808-6/447232820_485701057364609_6882034262964220786_n.jpg?_nc_cat=105&ccb=1-7&_nc_sid=a5f93a&_nc_eui2=AeHjriIDTRJC_PcvJAfgIouOFYiAgsWwUSoViICCxbBRKheimFO4y58FaaCHAq9X9ZazI6GTSJfowGXCwAWihCGQ&_nc_ohc=BKxVkhXz20EQ7kNvgHPdImd&_nc_zt=23&_nc_ht=scontent.fcgy2-4.fna&_nc_gid=Ai8oJezuBIWDV3zOmo6nf18&oh=00_AYC8O_UH06Ya04GiX9_yRbtBH9FcVgi15JusViQXFAD3pQ&oe=674B3112"
     }
 ]
+
+for post in blog_posts:
+    # Display the image
+    if "image" in post:
+        st.markdown(
+            f"""
+            <div style="display: flex; justify-content: center;">
+                <img src="{post['image']}" style="width: 350px; height: 350px; object-fit: cover;" />
+            </div>
+            """, 
+            unsafe_allow_html=True
+        )
+    
+    # Title for the content section
+    st.subheader("About Me")  # Added title for the content section
+    
+    # Display content in a structured format
+    for item in post["content"]:
+        st.write(f"**{item[0]}:** {item[1]}")
+    
+    st.markdown("---")
+    
